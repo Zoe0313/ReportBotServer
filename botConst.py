ccfaa817b2767f4e05b825d73f773b2a3703e378
Sixuan Yang <ysixuan@vmware.com>
2021-05-24 02:08:26 -0700

    Add slackbot const

---
+++ botConst.py
@@ -0,0 +1,32 @@
+import os
+
+# slack API base
+SLACK_API = "https://slack.com/api/"
+
+# bugzilla API
+BugzillaBase = "https://bugzilla.eng.vmware.com/show_bug.cgi?id="
+
+# bugzilla bug detail API
+QueryBugDetailURL = "https://bugzilla-rest.eng.vmware.com/rest/v1/bug/"
+
+# Authorization
+AUTH = "xoxb-2154537752-833403957187-yPWkRumT1Ayc3jq76H4TsviU"
+
+# content type
+CONTENT_TYPE_JSON_UTF = "Content-type: application/json;charset=utf-8"
+CONTENT_TYPE_URLENCODE = "Content-type: application/x-www-form-urlencoded"
+CONTENT_TYPE_JSON = "Content-type: application/json"
+
+# channel id
+# vSAN manager channel
+VSAN_70U3_CHANNEL = "G01KKSLTR1T",
+
+# vSAN channel-bot
+VSAN_SHANGHAI_BOT_CHANNEL = "G9NSYC9K7",
+
+# test channel
+TEST_YSIXUAN_CHANNEL = "C020CL1F00P",
+
+TEST_DDA_CHANNEL = "GQV757T6K",
+
+TEST_CHJING_CHANNEL = "G012BLW9C6R"
